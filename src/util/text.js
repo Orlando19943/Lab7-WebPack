ccb65326b64ccf32fd2991e797ea1d8afc36f0f9
Orlando <orlando.osberto@gmail.com>
2021-03-04 17:26:26 -0600

    Revert "Lab 7 con Webpack, el commit anterior era una version anterior"
    This reverts commit 87bfdbac25c7a56211e4081d59dff09c69ac63e8.

--- src/util/text.js
+++
@@ -1,8 +0,0 @@
-import {text} from "./historia"
-import {title} from "./titulos"
-function page(pagina){
-    document.getElementById("titulo").innerHTML = title[pagina - 1];
-    document.getElementById("historia").innerHTML = text[pagina - 1];
-}
-
-export {page};
