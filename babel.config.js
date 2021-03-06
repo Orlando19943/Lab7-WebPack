f87ce58a8f576cfa5fcfcff81fd8653d77543874
Orlando <orlando.osberto@gmail.com>
2021-03-05 18:07:49 -0600

    Babel sin WebPack

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,8 @@ const presets = [
     ],
   ];
 const plugins = [
-    "@babel/plugin-transform-arrow-functions"
+    "@babel/plugin-transform-arrow-functions",
+    "@babel/plugin-transform-template-literals"
 ];
   
 module.exports = { presets, plugins };
--- src/util/page.js
+++
@@ -1,6 +0,0 @@
-function page(pagina) {
-    console.debug("entra")
-    localStorage.setItem("numero", pagina);
-    window.open("historia.html");
-}
-export {page};
